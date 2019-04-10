3d7515327f898b93e779ea964d8eda670142fcba
sophiesong <song.yiy@husky.neu.edu>
2019-04-09 19:49:29 -0700

    log

--- src/app/views/user/login/login.component.ts
+++ src/app/views/user/login/login.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 import { Router} from '@angular/router';
 
@@ -8,46 +9,39 @@ import { ViewChild } from '@angular/core';
 import {SharedService} from '../../../services/shared.service.client';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  @ViewChild('f') myLoginForm: NgForm;
-  username: string;
-  password: string;
+    @ViewChild('f') myLoginForm: NgForm;
+    username: string;
+    password: string;
 
-  errorFlag: boolean;
-  errorMsg = 'Invalid username or password !';
+    errorFlag: boolean;
+    errorMsg = 'Invalid username or password !';
 
-  constructor(private router: Router, private _userService: UserService, private sharedService: SharedService) { }
+    constructor(private router: Router, private _userService: UserService, private sharedService: SharedService) { }
 
-  ngOnInit() {}
+    ngOnInit() {}
 
-  login() {
+    login() {
 
-    // fetching data from loginForm
-    this.username = this.myLoginForm.value.username;
-    this.password = this.myLoginForm.value.password;
+        // fetching data from loginForm
+        this.username = this.myLoginForm.value.username;
+        this.password = this.myLoginForm.value.password;
 
-      this._userService.login(this.username, this.password)
-          .subscribe(
-              (user: any) => {
-                  console.log(user);
-                  this.sharedService.user = user;
-                  this.router.navigate(['/profile']); },
-              (error: any) => {
-                  console.log(error);
-                  this.errorFlag = true;
-              }
-          );
-  }
-
-  // facebook() {
-  //     this._userService.facebook().subscribe((user: any) => {
-  //         console.log(user);
-  //         this.sharedService.user = user;
-  //     });
-  // }
+        this._userService.login(this.username, this.password)
+            .subscribe(
+                (user: any) => {
+                    console.log(user);
+                    this.sharedService.user = user;
+                    this.router.navigate(['/profile']); },
+                (error: any) => {
+                    console.log(error);
+                    this.errorFlag = true;
+                }
+            );
+    }
 
 }
